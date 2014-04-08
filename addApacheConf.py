6f35b3ab30f8540398e7e1dc38ec4022a0b82f66
vic <vrootic@gmail.com>
2014-04-09 02:30:54 +0800

    Add a write-to-httpconf script. Add a server package which is not generalized.

---
+++ addApacheConf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+
+project_name = sys.argv[1]
+project_path = "/var/www/"
+
+target_path = "/etc/apache2/http.conf"
+target_path = "./test.txt"
+
+with open(target_path, "a+") as target_file:
+    target_file.write("\n")
+    target_file.write("<Directory " + project_path + project_name + "/server>\n")
+    target_file.write("  Order Deny, Allow\n")
+    target_file.write("  AllowOverride all\n")
+    target_file.write("  Allow from all\n")
+    target_file.write("</Directory>\n")
